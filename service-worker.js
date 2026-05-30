98c04cee0f44b63080658a3b9461fdb5d8fbfc3e
veval1985 <veval.gruber@gmail.com>
2026-05-30 09:26:03 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,3 @@
-const CACHE_NAME = "minis-app";
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll([
-      "/",
-      "/index.html",
-      "/style.css",
-      "/script.js"
-    ]))
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("install", () => {
+  console.log("installed");
 });
